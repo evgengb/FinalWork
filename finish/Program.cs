046c253276b534887749da75b19f677e73139dcd
Burikin Evgeni <evgen_gb@bk.ru>
2023-01-15 11:42:52 +0700

    method array

--- finish/Program.cs
+++ finish/Program.cs
@@ -1,2 +1,15 @@
 ﻿string[] array1 = new string[5] {"Hello", "98", ";-)", "78954", "0"};
 string[] array2 = new string[array1.Length];
+
+void SortingAnArray(string[] array1, string[] array2)
+{
+    int meaning = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[meaning] = array1[i];
+        meaning++;
+        }
+    }
+}
